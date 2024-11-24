2f8d6dfc3aad3a1c8614db97d2efddcfd7c276c6
Humbert <122186863+xsabreusx@users.noreply.github.com>
2024-11-24 13:04:23 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+const int MAX_SIZE = 100;
+
+template <typename T> 
+class H_Stack {
+private:
+T array[MAX_SIZE];
+T *top;
+T *start;
+
+public:
+
+bool is_full (){
+
+}
+
+bool is_empty(){
+
+}
+
+};
+
+////////////////// --- driver function --- /////////////////
+int main() {
+
+
+
+
+
+}
